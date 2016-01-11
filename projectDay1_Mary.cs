c6d7774478f30ce68a188de37a47516fcbcefb58
mwinkelman <marymwinkelman@gmail.com>
2016-01-10 21:53:13 -0500

    initial setup commit

---
+++ projectDay1_Mary.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.
+                //Console.WriteLine("Enter company name:");
+                //string companyName = Console.ReadLine();
+                //Console.WriteLine("Enter company address:");
+                //string companyAddress = Console.ReadLine();
+                //Console.WriteLine("Enter company phone number (xxx) xxx-xxxx:");
+                //string companyPhone = Console.ReadLine();
+                //Console.WriteLine("Enter company fax number (xxx) xxx-xxxx:");
+                //string companyFax = Console.ReadLine();
+                //Console.WriteLine("Enter company website URL:");
+                //string companyWebsite = Console.ReadLine();
+                //Console.WriteLine("Enter company Manager first name:");
+                //string managerFirstName = Console.ReadLine();
+                //Console.WriteLine("Enter company Manager last name:");
+                //string managerLastName = Console.ReadLine();
+                //Console.WriteLine("Enter company Manager Phone Number:");
+                //string managerPhone = Console.ReadLine();
+
+                //Console.WriteLine(companyName);
+                //Console.WriteLine("Address: " + companyAddress);
+                //Console.WriteLine("Phone: " + companyPhone);
+                //Console.WriteLine("Fax: " + companyFax);
+                //Console.WriteLine("Website: " + companyWebsite);
+                //Console.WriteLine("Manager: " + managerFirstName + managerLastName);
+                //Console.Write("Manager Phone: " + managerPhone);
+
+
+            //2.
+                //Console.WriteLine("Enter an integer: ");
+                //int firstInt = int.Parse(Console.ReadLine());
+                //Console.WriteLine("Enter another integer: ");
+                //int secondInt = int.Parse(Console.ReadLine());
+                //Console.WriteLine("Enter another integer: ");
+                //int thirdInt = int.Parse(Console.ReadLine());
+                //Console.WriteLine("Enter another integer: ");
+                //int fourthInt = int.Parse(Console.ReadLine());
+                //Console.WriteLine("Enter a final integer: ");
+                //int fifthInt = int.Parse(Console.ReadLine());
+                //Console.WriteLine("Sum: " + (firstInt + secondInt + thirdInt + fourthInt + fifthInt));
+
+            //3.
+                //Console.WriteLine("Enter circle's radius: ");
+                //double radius = Convert.ToDouble(Console.ReadLine());
+                //double perimeter = 2 * Math.PI * radius;
+                //double area = Math.PI * radius * radius;
+                //Console.WriteLine("Perimeter: " + (perimeter));
+                //Console.WriteLine("Area: " + (area));
+
+            //4.
+                //Console.WriteLine("\"Welcome to We Can Code IT\"");
+
+            //5.
+                //Console.WriteLine("Manager First Name: ");
+                //string managerFirstName = Console.ReadLine();
+                //Console.WriteLine("Manager Last Name: ");
+                //string managerLastName = Console.ReadLine();
+                //Console.WriteLine("Manager Phone Number: ");
+                //string managerPhone = Console.ReadLine();
+
+                //string format = "{0,-20} {1,-20} {2,20}";
+
+                //Console.WriteLine();
+                //Console.WriteLine(format, "First Name", "Last Name", "Phone");
+                //Console.WriteLine();
+                //Console.WriteLine(format, managerFirstName, managerLastName, managerPhone);
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
